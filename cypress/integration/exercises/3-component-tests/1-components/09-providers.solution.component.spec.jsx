8cff253677996053f4bc0ff329fb6c20edffbc80
Stefano Magni <nori.ste.magni@gmail.com>
2021-03-27 17:35:52 +0100

    Fixes

---
+++ cypress/integration/exercises/3-component-tests/1-components/09-providers.solution.component.spec.jsx
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+/// <reference types="cypress-wait-until" />
+
+/**
+ * Main goals
+ * - Wrap the VirtualList into a component that brings the items and set the selection into a state
+ * manager of choice (React Context, Redux, MobX, Recoil, Valtio, Zustand, etc. doesn't matter) and
+ * test the selection
+ */
